72ff6364e548a3abf734f49b187c4cc12792fce6
billy73480 <billy73480@gmail.com>
2017-02-21 21:36:17 +0800

    Add files via upload

---
+++ cnn (1).py
@@ -0,0 +1,75 @@
+# Import the modules
+import cv2
+from sklearn.externals import joblib
+from skimage.feature import hog
+import numpy as np
+from keras.models import model_from_json
+from PIL import Image                                                            
+import matplotlib.pyplot as plt                                                  
+import glob
+
+# Load the classifier
+#clf = joblib.load("digits_cls.pkl")
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+print("model complie")
+
+# Read the input image 
+im = cv2.imread("note_1.jpg")
+
+# Read image into 3D array
+'''
+imageFolderPath = '/home/B/Pictures/'
+imagePath = glob.glob(imageFolderPath+'/*.JPG') 
+
+im_array = numpy.array( [numpy.array(Image.open(imagePath[i]).convert('L'), 'f') for i in range(len(imagePath))] )
+'''
+
+# Convert to grayscale and apply Gaussian filtering
+im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+im_gray = cv2.GaussianBlur(im_gray, (3, 3), 0)
+#cv2.imshow(" cv2.GaussianBlur", im_gray)
+#cv2.waitKey()
+
+# Threshold the image
+ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+#cv2.imshow(" cv2.threshold", im_th)
+
+# Find contours in the image
+im_test ,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Get rectangles contains each contour
+rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+
+# For each rectangular region, calculate HOG features and predict
+# the digit using Linear SVM.
+for rect in rects:
+    # Draw the rectangles
+    cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+    # Make the rectangular region around the digit
+    leng = int(rect[3] * 1.6)
+    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+    roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+
+    # Resize the image
+    roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+    roi = cv2.dilate(roi, (3, 3))
+    # Calculate the HOG features
+    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), 	visualise=False)
+    #nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+    
+    #reshape for model input based on X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    roi = roi.reshape(1, 28, 28, 1)
+    nbr = loaded_model.predict_classes(roi, verbose = 1)
+    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+
+cv2.imshow("Resulting Image with Rectangular ROIs", im)
+cv2.waitKey()
