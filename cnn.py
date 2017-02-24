a0d3832fb26677bb51de11bed6f5b1aed3f2b39e
billy73480 <billy73480@gmail.com>
2017-02-24 08:52:07 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -36,8 +36,6 @@ im_array = numpy.array( [numpy.array(Image.open(imagePath[i]).convert('L'), 'f')
 # Convert to grayscale and apply Gaussian filtering
 #im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
-#cv2.imshow(" cv2.GaussianBlur", img_blur)
-#cv2.waitKey()
 
 # Threshold the image
 img_thr = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 3)
@@ -46,31 +44,32 @@ img_thr = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, c
 # Find contours in the image
 im_test ,ctrs, hier = cv2.findContours(img_thr.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Get rectangles contains each contour
+# Get rectangles that contain each contour
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
 
 # For each rectangular region, calculate HOG features and predict
 # the digit using Linear SVM.
 for rect in rects:
-    # Draw the rectangles
-    cv2.rectangle(img, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
-    # Make the rectangular region around the digit
-    leng = int(rect[3] * 1.6)
-    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-    roi = img_thr[pt1:pt1+leng, pt2:pt2+leng]
-
-    # Resize the image
-    roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
-    roi = cv2.dilate(roi, (3, 3))
-    # Calculate the HOG features
-    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), 	visualise=False)
-    #nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
-    
-    #reshape for model input based on X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    roi = roi.reshape(1, 28, 28, 1)
-    nbr = loaded_model.predict_classes(roi, verbose = 1)
-    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    if ((rect[2]+rect[3])*2)>40:
+        # Draw the rectangles
+        cv2.rectangle(img, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+        # Make the rectangular region around the digit
+        leng = int(rect[3] * 1.6)
+        pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+        pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+        roi = img_thr[pt1:pt1+leng, pt2:pt2+leng]
+        
+        # Resize the image
+        roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+        roi = cv2.dilate(roi, (3, 3))    
+        # Calculate the HOG features
+        roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), 	visualise=False)
+        #nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+        
+        #reshape for model input based on X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+        roi = roi.reshape(1, 28, 28, 1)
+        nbr = loaded_model.predict_classes(roi, verbose = 1)
+        cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey()
