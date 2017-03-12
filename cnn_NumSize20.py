145d857002ad664b96c5087c6984edff77744c85
billy73480 <billy73480@gmail.com>
2017-03-12 21:18:27 +0800

    Update cnn_NumSize20.py

--- cnn_NumSize20.py
+++ cnn_NumSize20.py
@@ -4,8 +4,8 @@ import numpy as np
 from sklearn.externals import joblib
 from skimage.feature import hog
 from keras.models import model_from_json
-from PIL import Image                                                            
-import matplotlib.pyplot as plt                                                  
+from PIL import Image
+import matplotlib.pyplot as plt
 import glob
 from os import listdir
 from os.path import isfile, join
@@ -26,19 +26,23 @@ print("model complie")
 # Input the image
 src_path = '/home/billy73480/Handwriting_Recognition/Pictures/'
 onlyfiles = [ f for f in listdir(src_path) if isfile(join(src_path, f)) ]
-imgs = np.empty(len(onlyfiles), dtype=object)
-for n in range(0, len(onlyfiles)):
-	print onlyfiles[n]
-	imgs[n] = cv2.imread( join(src_path, onlyfiles[n]) )
-	
-
+#imgs = np.empty(len(onlyfiles), dtype=object)
 res_path = '/home/billy73480/Handwriting_Recognition/Results/'
 long_side = 20
+accuracy = 0
+data_num = 0
 
-for n in range(0, len(onlyfiles)):
+for n in range(0, len(onlyfiles)):		
+	img = cv2.imread( join(src_path, onlyfiles[n]) )
+	print onlyfiles[n]
+	data = onlyfiles[n].replace('.jpg', '')
+	data_seg = len(data)
+	data_num += len(data)
+	verify = np.zeros((len(data), 3))
+
 
 	# Image pre-processing
-	img_gray = cv2.cvtColor(imgs[n], cv2.COLOR_BGR2GRAY)	
+	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
 	img_thr = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9, 4)
 	kernel = np.ones((2, 2), np.uint8)
@@ -54,6 +58,7 @@ for n in range(0, len(onlyfiles)):
 
 	# For each rectangular region, calculate HOG features and predict
 	# the digit using Linear SVM.
+	seq = 0
 	for rect in rects:
 		if ((rect[2]+rect[3])>20):
 
@@ -63,33 +68,52 @@ for n in range(0, len(onlyfiles)):
 			height = rect[3]
 
 			# Draw the rectangles
-			cv2.rectangle(imgs[n], (pt_col, pt_row), (pt_col + width, pt_row + height), (0, 255, 0), 3)
+			cv2.rectangle(img, (pt_col, pt_row), (pt_col + width, pt_row + height), (0, 255, 0), 3)
 			roi = img_dil[pt_row:pt_row+height, pt_col:pt_col+width]
-		
+
 			# Create a black image
 			roi_black = np.zeros((28, 28), np.uint8)
-		
-			# Resize the image		
+
+			# Resize the image
 			if width < height:
 				width = width * long_side / height
-				height = long_side			
+				height = long_side
 			elif width > height:
 				height = height * long_side / width
-				width = long_side		
+				width = long_side
 			else:
 				width = long_side
-				height = long_side			
+				height = long_side
 
 			width_st = (28-width)/2
-			height_st = (28-height)/2		
+			height_st = (28-height)/2
 			roi = cv2.resize(roi, (width, height), interpolation = cv2.INTER_LINEAR)
 			roi_black[height_st : height_st+height, width_st : width_st+width] = roi
-			
+
 			#reshape for model input based on X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
 			roi_black = roi_black.reshape(1, 28, 28, 1)
 			nbr = loaded_model.predict_classes(roi_black, verbose = 1)
-			cv2.putText(imgs[n], str(int(nbr[0])), (rect[0], rect[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+			cv2.putText(img, str(nbr[0]), (pt_col, pt_row), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+			
+			verify[seq] = [pt_col, pt_row, nbr[0]]
+			seq += 1
 
-	cv2.imwrite(res_path + 'result_%d.png'%n, imgs[n])
+	# sort
+	def getKey(item):
+		return item[0]
+	verify =  sorted(verify, key = getKey)
+	
+	for n in range(0, len(data)):		
+		if int(data[n]) == verify[n][2]:
+			accuracy += 1			
+
+
+	cv2.imwrite(res_path + data + '.png', img)
 	#cv2.imshow("Resulting Image with Rectangular ROIs", img)
 	#cv2.waitKey()
+
+
+acu_rate = float(accuracy) / data_num
+print 'Total number of data: ' + str(data_num)
+print 'Total number of correctly recognized: ' + str(accuracy)
+print 'Accuracy: ' + str(acu_rate)
